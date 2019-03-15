3fc7d768d3db8aed5796a97e17eca701dd20019e
EduardoGarcia32 <slender560@gmail.com>
2019-03-14 18:55:39 -0600

    Convertir

--- src/reto5/conversiones_numericas.java
+++ src/reto5/conversiones_numericas.java
@@ -5,10 +5,31 @@
  */
 package reto5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Eduardo Garcia
  */
 public class conversiones_numericas {
-    
+  
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in);
+        int numeroconv;
+        String numeroconv2;
+        int result;
+        String son;
+        int repetir = 1;
+        
+        System.out.println("** Programa de conversiones para triangulos**");
+        System.out.println("a. Convertir de decimal a binario");
+        System.out.println("b. Convertir de decimal a octal");
+        System.out.println("c. Convertir de decimal a hexadecimal");
+        System.out.println("d. Convertir de binario a decimal");
+        System.out.println("e. Convertir de octal a decimal");
+        System.out.println("f. Convertir de hexadecimal a decimal");
+        
+    }     
 }
